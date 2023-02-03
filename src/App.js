7c480f98b397346614fd2cb2da3ed03f72612571
AhmedsGitStart <skkhubaibahmed0111@gmail.com>
2023-02-03 11:06:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { HomePage } from './HomePage';
+
+function App() {
+  return (
+    <div className="main-component">
+     <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
